cbb0c9d93478cbf1044ceb8788712663d0678dfb
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-04-15 10:49:55 +0200

    resolve conflicts

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import {Container, Typography} from "@mui/material";
+
+const Navigation = () => {
+    return <Container maxWidth="xl">
+        <Typography>Navigation bar section</Typography>
+    </Container>
+
+}
+
+export default Navigation;
